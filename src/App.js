8cba9126d2922d9f0985142d90831f6d4f8f43e0
yawsaii <bernardsaii20@gmail.com>
2021-09-16 22:27:26 +0000

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+import Header from "./components/header/Header";
+import List from "./components/list/List";
+import Map from "./components/map/Map";
+import { getPlacesData, getWeatherData } from "./api/index";
+// import { FilterNoneRounded } from "@material-ui/icons";
 
-function App() {
+const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+
+  //getting user current location
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  //getting the data from the api
+  useEffect(() => {
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
+
+      //for the weather
+      getWeatherData(coordinates.lat, coordinates.lng).then((data) => {
+        setWeatherData(data);
+      });
+
+      getPlacesData(type, bounds.ne, bounds.sw).then((data) => {
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setIsLoading(false);
+        setFilteredPlaces([]);
+      });
+    }
+  }, [type, bounds]);
+
+  //filtering by rating
+  useEffect(() => {
+    const filteredPlaces = places.filter(
+      (place) => Number(place.rating) > rating
+    );
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <CssBaseline />
+      <Header setCoordinates={setCoordinates} />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={filteredPlaces.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Map
+            coordinates={coordinates}
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
+            weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/list/List.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState, createRef } from 'react';
+import {
+    CircularProgress,
+    Grid,
+    Typography,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    Select
+} from '@material-ui/core';
+import PlaceDetails from '../placeDetails/PlaceDetails';
+import useStyles from './styles'
+
+const List = ({places, childClicked, isLoading, type, setType, rating, setRating}) => {
+
+    const classes = useStyles();
+    const [elRefs, setElRefs] = useState([]);
+
+    useEffect(() => {
+        const refs = Array(places?.length).fill().map((_, index) => elRefs[index] || createRef());
+        setElRefs(refs)
+    }, [places])
+
+
+
+    return (
+        <div className={classes.container}>
+            <Typography variant='h4'>
+                Restaurants, Hotels, & Attractions around you
+            </Typography>
+            {isLoading ? (
+                <div className={classes.loading}>
+                    <CircularProgress size='5rem' />
+                </div>
+            ) : (
+                    <div>
+                       {/* for selecting places */}
+            <FormControl className={classes.formControl}>
+                <InputLabel id='type'>Type</InputLabel>
+                <Select id='type' value={type} onChange={e => setType(e.target.value)}>
+                    <MenuItem value='restaurants'>Restaurants</MenuItem>
+                    <MenuItem value='hotels'>Hotels</MenuItem>
+                    <MenuItem value='attractions'>Attractions</MenuItem>
+                </Select>
+            </FormControl>
+
+            {/* for the rating */}
+            <FormControl className={classes.formControl}>
+                <InputLabel id='rating'>Rating</InputLabel>
+                <Select id='rating' value={rating} onChange={e => setRating(e.target.value)}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3.0</MenuItem>
+                    <MenuItem value={4}>Above 4</MenuItem>
+                    <MenuItem value={4.5}>Above 4.5</MenuItem>
+                </Select>
+            </FormControl>
+
+            {/* The places */}
+                <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, index) => (
+                    <Grid ref={elRefs[index]} key={index} item  xs={12}>
+                        <PlaceDetails 
+                            place={place}
+                            selected={Number(childClicked) === index}
+                            refProp={elRefs[index]}
+                        />
+                    </Grid>
+                ))
+                 }
+            </Grid> 
+                    </div>
+            )}  
+        </div>
+    )
+}
+
+export default List
